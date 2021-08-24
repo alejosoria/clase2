34e3c62a4a75e839f92e78a8997164a6bc23a8bc
Alejo Soria <alejosoria2020@gmail.com>
2021-08-24 20:42:39 -0300

    lo que sea

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace clase_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hola Wachin!");
         }
     }
 }
